ebb9cc277b9b6d16ddfb64b5fc690c04f07ae89d
BorovskyDL@hotmail.com <BorovskyDL@hotmail.com>
2020-02-25 12:20:52 +0300

    First project iteration

---
+++ Controllers/QuadraticEquationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace QuadraticEquationProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuadraticEquationController : ControllerBase
+    {
+        private readonly ILogger<QuadraticEquationController> _logger;
+        public QuadraticEquationController(ILogger<QuadraticEquationController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Type values at the end of URL string to perform a calculation. Input example: '/A,B,C'";
+        }
+        [HttpGet("{A},{B},{C}")]
+        public ActionResult<string> Get(double A, double B, double C)
+        {
+            QuadraticEquation Equation = new QuadraticEquation();
+            return Equation.EquationCalculation(A, B, C);
+        }
+    }
+}
---
+++ QuadraticEquation.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace QuadraticEquationProject
+{
+    public class QuadraticEquation
+    {
+        public double A { get; set; }
+
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public double D;
+
+        private double[] X = new double[2];
+        public string EquationCalculation(double A, double B, double C)
+        {
+            if (A == 0)
+            {
+                return ("First term coefficient must be not 0");
+            }
+            else
+            {
+                D = B * B - 4 * A * C;
+                if (D < 0)
+                {
+                    return "This equation has no valid roots";
+                }
+                if (D == 0)
+                {
+                    X[0] = ((-1) * B) / (2 * A);
+                    if (X[0] == 0)
+                        X[0] = 0;
+
+                    return String.Format("This equation has one valid root: {0}", X[0]);
+                }
+                else
+                {
+                    X[0] = ((-1) * B - Math.Sqrt(D)) / (2 * A);
+                    X[1] = ((-1) * B + Math.Sqrt(D)) / (2 * A);
+
+                    return String.Format("This equation has two valid roots: {0}, {1}", X[0], X[1]);
+                }
+            }
+        }
+    }
+}
